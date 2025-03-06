23307105e7556bb3a2fe510c3f257ff4c5d0589a
AA4598 <76053158+AA4598@users.noreply.github.com>
2025-03-06 11:33:55 +0200

    8th

--- backend/cypress/e2e/test.cy.js
+++ backend/cypress/e2e/test.cy.js
@@ -1,6 +1,6 @@
 describe("Landing Page", () => {
     it("should display 'filter shown with:' text", () => {
-      cy.visit("http://localhost:3000/info"); // Ensure the app is running
+      cy.visit("http://localhost:3000/"); // Ensure the app is running
       //cy.visit("/");
       //cy.contains("filter shown with:").should("be.visible");
       cy.contains("Phonebook").should("be.visible");
