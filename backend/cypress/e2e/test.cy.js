b0c7a901a88e3e6eaf618dc8bd82b1f0548ad5a5
AA4598 <76053158+AA4598@users.noreply.github.com>
2025-03-06 02:59:03 +0200

    8th

--- backend/cypress/e2e/test.cy.js
+++ backend/cypress/e2e/test.cy.js
@@ -1,7 +1,7 @@
 describe("Landing Page", () => {
     it("should display 'filter shown with:' text", () => {
-      //cy.visit("http://localhost:3000"); // Ensure the app is running
-      cy.visit("/");
+      cy.visit("http://localhost:3000"); // Ensure the app is running
+      //cy.visit("/");
       cy.contains("filter shown with:").should("be.visible");
     });
   });
