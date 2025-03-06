455e2b01d865e48807afebfad5af75a06b8d2895
Timo Laakkonen <AA4598@student.jamk.fi>
2025-03-06 13:04:07 +0200

    6th

--- backend/cypress/e2e/test.cy.js
+++ backend/cypress/e2e/test.cy.js
@@ -1,13 +1,14 @@
 describe("Landing Page", () => {
-    it("should display 'filter shown with:' text", () => {
-      cy.visit("http://localhost:3000");      
-      cy.contains("filter shown with:").should("be.visible");      
-    });
+  it("should display 'filter shown with:' text", () => {
+    cy.visit("http://localhost:3000");
+    //cy.contains("filter shown with:").should("be.visible");
+    cy.contains("filter shown with:", { timeout: 10000 }).should("be.visible");
   });
-  
-  describe("Info Page", () => {
-    it("should display 'filter shown with:' text", () => {
-      cy.visit("http://localhost:3000/info");
-      cy.contains("Phonebook has info for").should("be.visible");
-    });
+});
+
+describe("Info Page", () => {
+  it("should display 'filter shown with:' text", () => {
+    cy.visit("http://localhost:3000/info");
+    cy.contains("Phonebook has info for").should("be.visible");
   });
+});
