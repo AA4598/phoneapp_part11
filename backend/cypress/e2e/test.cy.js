1a0fb99f7edb32094b8a9e509d02e6ff302f5b13
Timo Laakkonen <AA4598@student.jamk.fi>
2025-03-06 13:34:01 +0200

    6th

--- backend/cypress/e2e/test.cy.js
+++ backend/cypress/e2e/test.cy.js
@@ -1,3 +1,12 @@
+describe("Landing Page Debug", () => {
+  it("should log the page content", () => {
+    cy.request("http://localhost:3000").then((response) => {
+      cy.log(response.body); // Logs HTML content
+      console.log(response.body); // Prints to Cypress console
+    });
+  });
+});
+
 describe("Landing Page", () => {
   it("should display 'filter shown with:' text", () => {
     cy.visit("http://localhost:3000");
