5122efd165c12136cf731083a60f6f4e2131f8d3
AA4598 <76053158+AA4598@users.noreply.github.com>
2025-03-06 02:44:42 +0200

    7th

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here      
     },
   },
 });
